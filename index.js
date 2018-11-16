8d15c564d42d92bea4990c7d78c7cefd69f639cc
Vissarion Tsitoglou <vissarionas@gmail.com>
2018-11-16 19:51:06 +0100

    Separate authentication

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-var express = require('express');
-var cors = require('cors');  
-var morgan = require('morgan'); 
-var passport = require('passport');
-var compression = require('compression');
-var FacebookTokenStrategy = require('passport-facebook-token');
+const express = require('express');
+const cors = require('cors');  
+const morgan = require('morgan'); 
+const compression = require('compression');
+const passport = require('passport');
+const authentication = require('./src/authentication');
 
-require('dotenv').config();
-
-passport.use(new FacebookTokenStrategy({
-  clientID: process.env.FB_CLIENT_ID,
-  clientSecret: process.env.FB_CLIENT_SECRET,
-}, (accessToken, refreshToken, profile, done) => {
-  let user = {
-    'name': profile.name.givenName + ' ' + profile.name.familyName,
-    'id': profile.id,
-    'token': accessToken
-  };
-  return done(null, user, null);
-}));
-
-var app = express();
+const app = express();
 
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(morgan("common"));
@@ -28,7 +14,6 @@ app.use(cors({
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -36,21 +21,8 @@ app.get('/', (req, res) => {
   res.send('HELLO WORLD!!');
 });
 
-app.get('/auth/facebook/token',
-  (req, res) => {
-    passport.authenticate('facebook-token', (err, user, info) => {
-      if (err) {
-        if (err.oauthError) {
-          var oauthError = JSON.parse(err.oauthError.data);
-          res.status(401).send(oauthError.error.message);
-        } else {
-          res.send(err);
-        }
-      } else {
-        // res.set('Transfer-Encoding', 'chunked');
-        res.status(200).send(user);
-      }
-  })(req, res);
+app.get('/auth/facebook/token', (req, res) => {
+    authentication.authenticate(req, res);
 });
 
 app.listen(3000, () => console.log('Server started on http://127.0.0.1:3000'));
