2cb2240ae5c7a4ec2d361f0310832a34d95bce72
Vissarion Tsitoglou <vissarion.tsitoglou@ibm.com>
2018-11-30 21:32:33 +0100

    Refactor registration function

---
+++ config.js
@@ -0,0 +1,5 @@
+const JWT_SECRET = 'This is the secret'
+
+module.exports = {
+  JWT_SECRET
+}
--- src/database.js
+++ src/database.js
@@ -1,20 +1,26 @@
 const NodeCouchDb = require('node-couchdb');
 
 const couch = new NodeCouchDb();
- 
-function getUsers(req, res) {
-  couch.get("users", "c3b04eec4bed82351a34561469000156").then(({data, headers, status}) => {
-    res.send(data);
-  }, err => {
-    res.send(err);
-  });
-}
 
-function saveUser(user) {
-  console.log(user.name, 'saving user to db');
-}
+const saveUser = (user) => {
+  return new Promise(function (resolve, reject) {
+    couch.insert("users", {
+      _id: Date.now().toString(),
+      userName: user.userName,
+      email: user.email,
+      password: user.password,
+      active: user.active,
+      provider: user.provider,
+      providerUserId: user.providerUserId
+  
+      }).then(({data, headers, status}) => {
+        resolve(data)
+      }, err => {
+        reject(err)
+    });
+  });
+};
 
 module.exports = {
-  getUsers: getUsers,
-  saveUser: saveUser
+  saveUser
 }
