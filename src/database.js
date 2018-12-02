e735056801361648078a7f7c1e152e5bf3e08741
Vissarion Tsitoglou <vissarion.tsitoglou@ibm.com>
2018-12-02 12:35:25 +0100

    Add user email check on register

--- src/database.js
+++ src/database.js
@@ -1,22 +1,44 @@
 const NodeCouchDb = require('node-couchdb');
+const config = require('config');
 
-const couch = new NodeCouchDb();
+require('dotenv').config();
+
+const couch = new NodeCouchDb({
+  auth: {
+    user: process.env.DB_USERNAME,
+    pass: process.env.DB_PASSWORD
+  },
+});
 
 const saveUser = (user) => {
-  return new Promise(function (resolve, reject) {
-    couch.insert("users", {
-      _id: Date.now().toString(),
-      userName: user.userName,
-      email: user.email,
-      password: user.password,
-      active: user.active,
-      provider: user.provider,
-      providerUserId: user.providerUserId
+  const dbName = config.get('couch.databases.users');
+  const viewUrl = config.get('couch.views.usersByEmail');
   
-      }).then(({data, headers, status}) => {
-        resolve(data)
-      }, err => {
-        reject(err)
+  return new Promise(function (resolve, reject) {
+
+    couch.get(dbName, viewUrl, {key: user.email})
+    .then(({data, headers, status}) => {
+      if (!data.rows.length) {
+        couch.insert("users", {
+          _id: Date.now().toString(),
+          userName: user.userName,
+          email: user.email,
+          password: user.password,
+          active: user.active,
+          provider: user.provider,
+          providerUserId: user.providerUserId
+          }).then(({data, headers, status}) => {
+            resolve(data);
+          }, err => {
+            reject(err);
+            console.log(err);
+        });
+      } else {
+        reject(`Email ${user.email} already exists`);
+      }
+    }, err => {
+      reject(err);
+      console.log(err);
     });
   });
 };
