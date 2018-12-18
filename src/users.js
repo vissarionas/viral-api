345b0a96201011f906daa4bfc1400ab14b53eac4
Vissarion Tsitoglou <vissarionas@gmail.com>
2018-12-18 15:07:51 +0100

    Refactor mongoClient.connection

--- src/users.js
+++ src/users.js
@@ -10,108 +10,85 @@ const client = new MongoClient(config.get('uri'), {
   }
 });
 
+client.connect()
+.then(() => {
+  console.log('MongoDb connection opened');
+}, err => {
+  console.log(err);
+});
+
 const getUserById = (req, res, id) => {
-  client.connect()
-  .then(() => {
-    const db = client.db(dbName);
-    db.collection(config.get('collections.users')).findOne({_id: id})
-    .then(profile => res.status(200).send(profile));
-  }, err => {
-    console.log(err);
-  });
+  const db = client.db(dbName);
+  db.collection(config.get('collections.users')).findOne({_id: id})
+  .then(profile => res.status(200).send(profile));
 };
 
 const getUserByFacebookId = (facebookId) => {
   return new Promise(function (resolve, reject) {
-    client.connect()
-    .then(() => {
-      const db = client.db(dbName);
-      db.collection(config.get('collections.users')).findOne({facebookId: facebookId})
-      .then(userObject => {
-        resolve(userObject);
-        client.close();
-      }, err => {
-        reject(err);
-        client.close();
-      });
+    const db = client.db(dbName);
+    db.collection(config.get('collections.users')).findOne({facebookId: facebookId})
+    .then(userObject => {
+      resolve(userObject);
+    }, err => {
+      reject(err);
     });
   });
 };
 
 const getUserByEmail = (email) => {
   return new Promise(function (resolve, reject) {
-    client.connect()
-    .then(() => {
-      const db = client.db(dbName);
-      db.collection('users').findOne({
-        email: email
-      })
-      .then(userObject => {
-        if (userObject) {
-          resolve(userObject);
-          // client.close();
-        } else {
-          reject(null);
-          // client.close();
-        };
-      }, err => {
-        reject(err);
-        // client.close();
-      });
+    const db = client.db(dbName);
+    db.collection('users').findOne({email: email})
+    .then(userObject => {
+      if (userObject) {
+        resolve(userObject);
+      } else {
+        reject(null);
+      };
+    }, err => {
+      reject(err);
     });
   });
 };
 
 const saveEmailUser = (user) => {
   return new Promise(function (resolve, reject) {
-    client.connect()
-    .then(() => {
-      const db = client.db(dbName);
-      db.collection('users').findOne({email: user.email})
-      .then(userObject => {
-        if (!userObject) {
-          db.collection('users').insertOne(user)
-          .then((data) => {
-            resolve(data);
-            client.close();
-          }, err => {
-            reject(err);
-            client.close();
-          });
-        } else {
-          resolve(userObject);
-        }
-      }, err => {
-        reject(err);
-        client.close();
-      });
+    const db = client.db(dbName);
+    db.collection('users').findOne({email: user.email})
+    .then(userObject => {
+      if (!userObject) {
+        db.collection('users').insertOne(user)
+        .then((data) => {
+          resolve(data);
+        }, err => {
+          reject(err);
+        });
+      } else {
+        resolve(userObject);
+      }
+    }, err => {
+      reject(err);
     });
   });
 };
 
 const saveFacebookUser = (user) => {
   return new Promise(function (resolve, reject) {
-    client.connect()
-    .then(() => {
-      const db = client.db(dbName);
-      db.collection('users').findOne({facebookId: user.facebookId})
-      .then(userObject => {
-        if (!userObject) {
-          db.collection('users').insertOne(user)
-          .then(() => {
-            resolve(user);
-            client.close();
-          }, err => {
-            reject(err);
-            client.close();
-          });
-        } else {
-          resolve(userObject);
-        }
-      }, err => {
-        reject(err);
-        client.close();
-      });
+    const db = client.db(dbName);
+    db.collection('users').findOne({facebookId: user.facebookId})
+    .then(userObject => {
+      if (!userObject) {
+        db.collection('users').insertOne(user)
+        .then(() => {
+          resolve(user);
+        }, err => {
+          reject(err);
+        });
+      } else {
+        resolve(userObject);
+      }
+    }, err => {
+      reject(err);
     });
   });
 };
