70338f4568b63746000b217ac5357af6cc926e78
Vissarion Tsitoglou <vissarionas@gmail.com>
2018-12-21 15:23:58 +0100

    Add mixed location scenarios

--- test/coordinates-test.js
+++ test/coordinates-test.js
@@ -4,8 +4,11 @@ const assert = require('chai').assert;
 
 describe ('**createCoordinatesBox**', function () {
   const normalLocation = { lon: 50, lat: 100 };
+  const normalLocationNegative = { lon: -50, lat: -100 };
   const extremeLocation = { lon: 89, lat: 179 };
   const extremeLocationNegative = { lon: -89, lat: -179 };
+  const mixPositiveNegative = { lon: 2, lat: -179};
+  const mixNegativePositive = { lon: -2, lat: 179};
   const increment = 5;
   it('should be an array of five', () => {
     const result = coordinates.createCoordinatesBox(normalLocation, increment);
@@ -20,6 +23,15 @@ describe ('**createCoordinatesBox**', function () {
     assert.deepEqual(result[2], [45, 105]);
     assert.deepEqual(result[3], [45, 95]);
     assert.deepEqual(result[4], [55, 95]);
+  });
+
+  it('should match negative hardcoded results', () => {
+    const result = coordinates.createCoordinatesBox(normalLocationNegative, increment);
+    assert.deepEqual(result[0], [-45, -105]);
+    assert.deepEqual(result[1], [-45, -95]);
+    assert.deepEqual(result[2], [-55, -95]);
+    assert.deepEqual(result[3], [-55, -105]);
+    assert.deepEqual(result[4], [-45, -105]);
   });
 
   it('should match hardcoded results on extreme locations', () => {
@@ -39,4 +51,24 @@ describe ('**createCoordinatesBox**', function () {
     assert.deepEqual(result[3], [-90, 176]);
     assert.deepEqual(result[4], [-80, 176]);
   });
+
+  it('should match hardcoded results on mixPositiveNegative locations', () => {
+    const result = coordinates.createCoordinatesBox(mixPositiveNegative, increment);
+    assert.deepEqual(result[0], [7, 176]);
+    assert.deepEqual(result[1], [7, -174]);
+    assert.deepEqual(result[2], [-3, -174]);
+    assert.deepEqual(result[3], [-3, 176]);
+    assert.deepEqual(result[4], [7, 176]);
+  });
+
+  it('should match hardcoded results on mixNegativePositive locations', () => {
+    const result = coordinates.createCoordinatesBox(mixNegativePositive, increment);
+    assert.deepEqual(result[0], [3, 174]);
+    assert.deepEqual(result[1], [3, 4]);
+    assert.deepEqual(result[2], [-7, 4]);
+    assert.deepEqual(result[3], [-7, 174]);
+    assert.deepEqual(result[4], [3, 174]);
+  });
+
+
 });
