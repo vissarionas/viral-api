059473e9f8063e8fda609650fc8ddfd872af2aa6
Vissarion Tsitoglou <vissarion.tsitoglou@ibm.com>
2018-12-21 20:16:33 +0100

    Add getIntersectedPosts endpoint

--- src/coordinates.js
+++ src/coordinates.js
@@ -3,10 +3,10 @@ const createCoordinatesBox = (location, increment) => {
   // Prevent exceeeding poles
   location.lon = location.lon > 90 - increment ? 90 - increment : location.lon;
   location.lon = location.lon < -90 + increment ? -90 + increment : location.lon;
-
+  // Create a box(Polygon) surrounding post's location point
   coordinatesBox.push(
     [
-      // TOP LEFT
+      // top left
       location.lon + increment,
       location.lat - increment < -180
       ? 180 - (increment - (180 - (location.lat * -1)))
@@ -14,24 +14,24 @@ const createCoordinatesBox = (location, increment) => {
         
     ],
     [
-      // TOP RIGHT
+      // top right
       location.lon + increment,
       location.lat + increment > 180 ? increment - (180 - location.lat) : location.lat + increment
     ],
     [
-      // BOTTOM RIGHT
+      // bottom right
       location.lon - increment,
       location.lat + increment > 180 ? increment - (180 - location.lat) : location.lat + increment
     ],
     [
-      // BOTTOM LEFT
+      // bottom left
       location.lon - increment,
       location.lat - increment < -180
       ? 180 - (increment - (180 - (location.lat * -1)))
       : location.lat - increment
     ],
     [
-      // TOP LEFT
+      // top left
       location.lon + increment,
       location.lat - increment < -180
       ? 180 - (increment - (180 - (location.lat * -1)))
--- src/posts.js
+++ src/posts.js
@@ -1,10 +1,22 @@
 const config = require('config').mongo;
 const dbName = config.get('databases.viral');
 const client = require('./dbConnection');
+const coordinates = require('./coordinates');
 
-const getAllPosts = (req, res) => {
+const getIntersectedPosts = (req, res) => {
   const db = client.db(dbName);
-  const postsCursor = db.collection(config.get('collections.posts')).find();
+  const longitude = req.body.longitude;
+  const latitude = req.body.latitude;
+  const postsCursor = db.collection(config.get('collections.posts')).find({
+    geo: {
+      $geoIntersects: {
+        $geometry: {
+          type: "Point",
+          coordinates: [Number(longitude), Number(latitude)]
+        }
+      }
+    }
+  });
   let posts = [];
   postsCursor.forEach(doc => {
     posts.push(doc);
@@ -14,16 +26,19 @@ const getAllPosts = (req, res) => {
 };
 
 const createPostObject = (body, location) => {
+  const coordinateBox = coordinates.createCoordinatesBox(location, 0.05)
   const postObject = {
     "_id": "something",
     "user": "some user id",
     "body": body,
     "geo": {
       "type": "Polygon",
-      "coordinates": []
+      "coordinates": [
+        coordinateBox
+      ]
     }
   }
-  return createPostObject;
+  return postObject;
 };
 
 const testFunction = () => {
@@ -31,7 +46,7 @@ const testFunction = () => {
 }
 
 module.exports = {
-  getAllPosts,
+  getIntersectedPosts,
   createPostObject,
   testFunction
 }
