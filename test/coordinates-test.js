126e6d29f383b4d0ad77ae9001f41392bbb87113
Vissarion Tsitoglou <vissarion.tsitoglou@ibm.com>
2018-12-29 21:19:20 +0200

    Create tests and functions for coordinatesBox scaling

--- test/coordinates-test.js
+++ test/coordinates-test.js
@@ -63,5 +63,130 @@ describe('coordinates', () => {
       assert.deepEqual(result[3], [-7, 174]);
       assert.deepEqual(result[4], [3, 174]);
     });  
+  }),
+  describe('getSidesToBeScaled', ()=> {
+    it('should equal UP_RIGHT', () => {
+      const newLocation = {lon: 17, lat: 48};
+      const previousLocation = {lon: 16, lat: 47};
+      const result = coordinates.getSidesToBeScaled(newLocation, previousLocation);
+      expect(result).to.be.string;
+      assert.equal(result, 'UP_RIGHT');
+    });
+    it('should equal UP_LEFT', () => {
+      const newLocation = {lon: 17, lat: 47};
+      const previousLocation = {lon: 16, lat: 48};
+      const result = coordinates.getSidesToBeScaled(newLocation, previousLocation);
+      expect(result).to.be.string;
+      assert.equal(result, 'UP_LEFT');
+    });
+    it('should equal UP_NULL', () => {
+      const newLocation = {lon: 17, lat: 48};
+      const previousLocation = {lon: 16, lat: 48};
+      const result = coordinates.getSidesToBeScaled(newLocation, previousLocation);
+      expect(result).to.be.string;
+      assert.equal(result, 'UP_NULL');
+    });
+    it('should equal NULL_NULL', () => {
+      const newLocation = {lon: 17, lat: 48};
+      const previousLocation = {lon: 17, lat: 48};
+      const result = coordinates.getSidesToBeScaled(newLocation, previousLocation);
+      expect(result).to.be.string;
+      assert.equal(result, 'NULL_NULL');
+    });
+    it('should equal DOWN_LEFT', () => {
+      const newLocation = {lon: 17, lat: 47};
+      const previousLocation = {lon: 18, lat: 48};
+      const result = coordinates.getSidesToBeScaled(newLocation, previousLocation);
+      expect(result).to.be.string;
+      assert.equal(result, 'DOWN_LEFT');
+    });
+    it('should equal DOWN_RIGHT', () => {
+      const newLocation = {lon: 16, lat: 48};
+      const previousLocation = {lon: 17, lat: 47};
+      const result = coordinates.getSidesToBeScaled(newLocation, previousLocation);
+      expect(result).to.be.string;
+      assert.equal(result, 'DOWN_RIGHT');
+    });
+    it('should equal DOWN_NULL', () => {
+      const newLocation = {lon: 17, lat: 48};
+      const previousLocation = {lon: 18, lat: 48};
+      const result = coordinates.getSidesToBeScaled(newLocation, previousLocation);
+      expect(result).to.be.string;
+      assert.equal(result, 'DOWN_NULL');
+    });
+  }),
+  describe('scaleCoordinatesBox', () => {
+    it('should scale the coordinates UP_RIGHT', () => {
+      const sides = 'UP_RIGHT';
+      const existingCoordinateBox = [
+        [55, 95],
+        [55, 105],
+        [45, 105],
+        [45, 95],
+        [55, 95]
+      ];
+      const result = coordinates.scaleCoordinatesBox(existingCoordinateBox, sides);
+      assert.deepEqual(result, [[60, 95], [60, 110], [45, 110], [45, 95], [60, 95]]);
+    });
+    it('should scale the coordinates UP_LEFT', () => {
+      const sides = 'UP_LEFT';
+      const existingCoordinateBox = [
+        [55, 95],
+        [55, 105],
+        [45, 105],
+        [45, 95],
+        [55, 95]
+      ];
+      const result = coordinates.scaleCoordinatesBox(existingCoordinateBox, sides);
+      assert.deepEqual(result, [[60, 90], [60, 105], [45, 105], [45, 90], [60, 90]]);
+    });
+    it('should scale the coordinates UP_NULL', () => {
+      const sides = 'UP_NULL';
+      const existingCoordinateBox = [
+        [55, 95],
+        [55, 105],
+        [45, 105],
+        [45, 95],
+        [55, 95]
+      ];
+      const result = coordinates.scaleCoordinatesBox(existingCoordinateBox, sides);
+      assert.deepEqual(result, [[60, 95], [60, 105], [45, 105], [45, 95], [60, 95]]);
+    });
+    it('should scale the coordinates DOWN_RIGHT', () => {
+      const sides = 'DOWN_RIGHT';
+      const existingCoordinateBox = [
+        [55, 95],
+        [55, 105],
+        [45, 105],
+        [45, 95],
+        [55, 95]
+      ];
+      const result = coordinates.scaleCoordinatesBox(existingCoordinateBox, sides);
+      assert.deepEqual(result, [[55, 95], [55, 110], [40, 110], [40, 95], [55, 95]]);
+    });
+    it('should scale the coordinates DOWN_LEFT', () => {
+      const sides = 'DOWN_LEFT';
+      const existingCoordinateBox = [
+        [55, 95],
+        [55, 105],
+        [45, 105],
+        [45, 95],
+        [55, 95]
+      ];
+      const result = coordinates.scaleCoordinatesBox(existingCoordinateBox, sides);
+      assert.deepEqual(result, [[55, 90], [55, 105], [40, 105], [40, 90], [55, 90]]);
+    });
+    it('should scale the coordinates DOWN_NULL', () => {
+      const sides = 'DOWN_NULL';
+      const existingCoordinateBox = [
+        [55, 95],
+        [55, 105],
+        [45, 105],
+        [45, 95],
+        [55, 95]
+      ];
+      const result = coordinates.scaleCoordinatesBox(existingCoordinateBox, sides);
+      assert.deepEqual(result, [[55, 95], [55, 105], [40, 105], [40, 95], [55, 95]]);
+    });
   });
 });
