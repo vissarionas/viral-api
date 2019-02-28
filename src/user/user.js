97a30cd738ec0eda431382de212c729860acc754
Vissarion Tsitoglou <vissarion.tsitoglou@ibm.com>
2019-02-28 19:42:35 +0100

    Refactor user class to user object

--- src/user/user.js
+++ src/user/user.js
@@ -1,62 +1,58 @@
 /* eslint-disable consistent-return */
 const config = require('config').mongo;
-const getClient = require('../shared/database');
+const getCollection = require('../shared/collectionClient');
 
-class User {
-  constructor() {
-    (async () => {
-      this.collection = await getClient(config.get('collections.users'));
-    })();
-  }
+const User = {};
 
-  async getById(id) {
-    try {
-      await this.collection.findOne({ _id: id });
-    } catch (err) {
-      return Promise.reject(err);
-    }
-  }
+(async () => {
+  this.collection = await getCollection(config.get('collections.users'));
+})();
 
-  async getByEmail(email) {
-    try {
-      const user = await this.collection.findOne({ email });
-      if (!user) throw new Error('user does not exist');
-      return user;
-    } catch (err) {
-      return Promise.reject(err);
-    }
+User.getById = async (id) => {
+  try {
+    await this.collection.findOne({ _id: id });
+  } catch (err) {
+    return Promise.reject(err);
   }
+};
 
-  async getByFacebookId(facebookId) {
-    try {
-      const user = await this.collection.findOne({ facebookId });
-      if (!user) throw new Error('user does not exist');
-      return user;
-    } catch (err) {
-      return Promise.reject(err);
-    }
+User.getByEmail = async (email) => {
+  try {
+    const user = await this.collection.findOne({ email });
+    if (!user) throw new Error('user does not exist');
+    return user;
+  } catch (err) {
+    return Promise.reject(err);
   }
+};
 
-  async save(userObject) {
-    try {
-      await this.collection.insertOne(userObject);
-      return userObject;
-    } catch (err) {
-      return Promise.reject(err);
-    }
+User.getByFacebookId = async (facebookId) => {
+  try {
+    const user = await this.collection.findOne({ facebookId });
+    if (!user) throw new Error('user does not exist');
+    return user;
+  } catch (err) {
+    return Promise.reject(err);
   }
+};
 
-  async update(docIdentifier, property, value) {
-    try {
-      this.collection.updateOne({ docIdentifier }, { $set: { [property]: value } });
-    } catch (err) {
-      return Promise.reject(err);
-    }
+User.save = async (userObject) => {
+  try {
+    await this.collection.insertOne(userObject);
+    return userObject;
+  } catch (err) {
+    return Promise.reject(err);
   }
+};
 
-  isFacebookUser(userObject){
-    return userObject.facebookId;
+User.update = async (docIdentifier, property, value) => {
+  try {
+    this.collection.updateOne({ docIdentifier }, { $set: { [property]: value } });
+  } catch (err) {
+    return Promise.reject(err);
   }
-}
+};
+
+User.isFacebookUser = userObject => userObject.facebookId;
 
 module.exports = User;
