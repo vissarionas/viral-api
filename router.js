2b83e5960072024975dda2a5b47ceda54333a85a
Vissarion Tsitoglou <vissarion.tsitoglou@ibm.com>
2019-03-12 20:32:18 +0100

    Depromisify gets

--- router.js
+++ router.js
@@ -9,36 +9,11 @@ const rootRouter = express.Router();
 const externalAuthRouter = express.Router();
 const schema = require('./src/graphQL/schema');
 const user = require('./src/user');
-const post = require('./src/post');
-
-rootRouter.use(function timeLog(req, res, next) {
-  console.log('Middleware example. Time: ', Date.now());
-  next();
-});
-
-rootRouter.get('/', (req, res, next) => {
-  setTimeout(() => {
-    console.log('timeout');
-    next();
-  }, 5000);
-}, (req, res) => res.send('ROOT'));
 
 rootRouter.get('/graphql', passport.authenticate('jwt', { session: false }),
   graphqlHTTP({
     schema,
   }));
-
-rootRouter.get('/user', passport.authenticate('jwt', { session: false }), (req, res) => {
-  user.getUsers(req, res);
-});
-
-rootRouter.get('/post', passport.authenticate('jwt', { session: false }), (req, res) => {
-  post.getPosts(req, res);
-});
-
-rootRouter.get('/posts', passport.authenticate('jwt', { session: false }), (req, res) => {
-  posts.getIntersectedPosts(req, res);
-});
 
 rootRouter.post('/savePost', passport.authenticate('jwt', { session: false }), (req, res) => {
   posts.savePost(req, res);
